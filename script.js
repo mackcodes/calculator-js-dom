b6e201ec4de741fdacd7f8bbd170bec273302491
Mayank Kumar <mackcodes03@gmail.com>
2026-02-21 23:57:31 +0530

    js display

---
+++ script.js
@@ -0,0 +1,25 @@
+const nine = document.querySelector(".nine");
+const eight = document.querySelector(".eight");
+const seven = document.querySelector(".seven");
+const six = document.querySelector(".six");
+const five = document.querySelector(".five");
+const four = document.querySelector(".four");
+const three = document.querySelector(".three");
+const two = document.querySelector(".two");
+const one = document.querySelector(".one");
+const zero = document.querySelector(".zero");
+
+nine.addEventListener("click", () => handleClick(9));
+eight.addEventListener("click", () => handleClick(8));
+seven.addEventListener("click", () => handleClick(7));
+six.addEventListener("click", () => handleClick(6));
+five.addEventListener("click", () => handleClick(5));
+four.addEventListener("click", () => handleClick(4));
+three.addEventListener("click", () => handleClick(3));
+two.addEventListener("click", () => handleClick(2));
+one.addEventListener("click", () => handleClick(1));
+zero.addEventListener("click", () => handleClick(0));
+
+function handleClick(selection){
+    
+}
