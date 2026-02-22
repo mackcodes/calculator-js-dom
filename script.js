10f474e41a8592f4c33d6024f60d56a71254a27c
Mayank Kumar <mackcodes03@gmail.com>
2026-02-22 22:26:43 +0530

    added the feature to display the pressed buttons on the input display

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
-const nine = document.querySelector(".nine");
-const eight = document.querySelector(".eight");
-const seven = document.querySelector(".seven");
-const six = document.querySelector(".six");
-const five = document.querySelector(".five");
-const four = document.querySelector(".four");
-const three = document.querySelector(".three");
-const two = document.querySelector(".two");
-const one = document.querySelector(".one");
-const zero = document.querySelector(".zero");
+const inputDisplay = document.querySelector(".inputDisplay");
 
-nine.addEventListener("click", () => handleClick(9));
-eight.addEventListener("click", () => handleClick(8));
-seven.addEventListener("click", () => handleClick(7));
-six.addEventListener("click", () => handleClick(6));
-five.addEventListener("click", () => handleClick(5));
-four.addEventListener("click", () => handleClick(4));
-three.addEventListener("click", () => handleClick(3));
-two.addEventListener("click", () => handleClick(2));
-one.addEventListener("click", () => handleClick(1));
-zero.addEventListener("click", () => handleClick(0));
+// iterates through each buttons and select and display
+document.querySelectorAll(".button").forEach((button, index) => {
+    if(index == 0) return;
+    button.addEventListener("click", () =>{
+        inputDisplay.textContent+= button.textContent + " ";
+    })
+})
 
-function handleClick(selection){
-    
-}
