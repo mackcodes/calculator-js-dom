8708b45096c97bc087a99bead0029a84c1fcc461
Mayank Kumar <mackcodes03@gmail.com>
2026-02-23 23:51:26 +0530

    created function

--- script.js
+++ script.js
@@ -3,8 +3,26 @@ const inputDisplay = document.querySelector(".inputDisplay");
 // iterates through each buttons and select and display
 document.querySelectorAll(".button").forEach((button, index) => {
     if(index == 0) return;
-    button.addEventListener("click", () =>{
-        inputDisplay.textContent+= button.textContent + " ";
+    button.addEventListener("click", (e) =>{
+        inputDisplay.textContent+= e.target.textContent;  // e.target tells which button was clicked
     })
 })
 
+function addition(a, b){
+    return a + b;
+};
+
+function subtraction(a, b){
+    return a - b;
+};
+
+function multiplication(a, b){
+    return a * b;
+};
+
+function division(a, b){
+    if(b == 0) return "Cannot Divide by Zero";
+    return a / b;
+};
+
+kdfjkldfjk
